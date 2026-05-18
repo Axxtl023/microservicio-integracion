6406ad80ef6e8f85d7e30ee584272ddf172b9c50
Axel Paliz <aepaliz@puce.edu.ec>
2026-05-18 17:16:50 -0500

    Configuración de los cores para que exista comunicación con el front

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const rawOrigins = process.env.CORS_ORIGINS ?? 'http://localhost:5173';
-  const origins = rawOrigins.split(',').map((o) => o.trim());
-
   app.enableCors({
-    origin: origins,
-    methods: ['GET', 'HEAD'],
+    origin: 'https://booking-frontend-ashy.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
 
@@ -27,7 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT ?? 3003;
+  const port = process.env.PORT || 3003;
   await app.listen(port, '0.0.0.0');
   console.log(`Microservicio Integración corriendo en el puerto ${port}`);
 }
